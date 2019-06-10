e934fcf7484dc4164acef1051ce1537eecfcedf2
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-15-137-187.wireless.pacific.edu>
2019-06-10 13:52:22 -0700

    connected clicked post to edit expense page

--- src/components/EditPostPage.js
+++ src/components/EditPostPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PostForm from '../components/PostForm';
+import { connect } from 'react-redux';
 
 const EditPostPage = (props) => {
     return (
         <div>
-        <h1>Edit Post Page</h1>
+            <h1>Edit Post Page</h1>
+            {console.log(props.post)}
         </div>
     )
 };
 
-export default EditPostPage;
+const mapStateToProps = (state, props) => {
+    return {
+        post: state.posts.find((post) => post.id === props.match.params.id)
+    }
+}
+
+export default connect(mapStateToProps)(EditPostPage);
--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 export const PostListItem = ({id, title, body, timestamp}) => {
     return (
-        <div>
-            <Link to={`edit/${id}`}>
-                <span>{title}</span>
-            </Link>
-        </div>
+        <Link to={`edit/${id}`}>
+            <div>
+                <h3 className="list-item__title">{title}</h3>
+                <span className="list-item__sub-title">{moment(timestamp).format('MMMM Do, YYYY')}</span>
+            </div>
+        </Link>
     );
 };
